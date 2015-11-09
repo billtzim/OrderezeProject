6d59c3970d081f998c019bfef87919d7c133ba10
Vasileios Tzimourtos <vtzimourtos@windowslive.com>
2015-11-09 17:31:41 +0200

    Created functions for blob management in BlobStorageFunctions class blobClientConnect blobGetContainerRef blobGetBlobRef setPublicPermissions addBlobMetadata

--- OrderezeImagePanel/OrderezeImagePanel/BlobStorageFunctions.cs
+++ OrderezeImagePanel/OrderezeImagePanel/BlobStorageFunctions.cs
@@ -1,11 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+using System.Configuration;
 
 namespace OrderezeImagePanel
 {
     public class BlobStorageFunctions
     {
+        public CloudBlobClient blobClientConnect(string connstring)
+        {
+            // Retrieve storage account from connection string.
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(ConfigurationManager.AppSettings[connstring].ToString());
+            return storageAccount.CreateCloudBlobClient();
+        }
+
+        public CloudBlobContainer blobGetContainerRef(CloudBlobClient blobclient, string containerName)
+        {
+            // Retrieve reference to a previously created container.
+            CloudBlobContainer container = blobclient.GetContainerReference(containerName);
+            container.CreateIfNotExists();
+            container.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess = BlobContainerPublicAccessType.Container
+                });
+
+            return container;
+        }
+
+        public CloudBlockBlob blobGetBlobRef(CloudBlobContainer container, string blobname)
+        {
+            // Retrieve reference to a blob named
+            return container.GetBlockBlobReference(blobname);
+        }
+
+        public void setPublicPermissions(CloudBlobContainer container, BlobContainerPublicAccessType Level)
+        {
+            container.SetPermissions(
+                new BlobContainerPermissions
+                {
+                    PublicAccess = Level
+                });
+        }
+
+        public void addBlobMetadata(CloudBlockBlob blob, string metadataKey, string metadataValue)
+        {
+            blob.Metadata.Add(metadataKey, metadataValue);
+        }
+
     }
 }
