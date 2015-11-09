f13001e1cc36f7088ec2df18c6b3516d5d1ddab5
Vasileios Tzimourtos <vtzimourtos@windowslive.com>
2015-11-09 17:22:29 +0200

    Added wbconfig storage connectioin string parameter Added empty default.aspx page Added ImageService skeleton class to implement IImageService interface Added BlobStorage empty class for blob functions

---
+++ OrderezeImagePanel/OrderezeImagePanel/ImageService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using OrderezeTask;
+
+namespace OrderezeImagePanel
+{
+    public class ImageService : OrderezeTask.IImagesService
+    {
+        public int AddNewImage(ref Image image)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteImage(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Image> GetImages()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
